fd3156796c173ed833389e08c451c2a0b2434e78
Javier Jiménez <javier.jimenez98@hotmail.com>
2020-11-17 18:45:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/laboratorioaudio/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.laboratorioaudio;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity{
+    ImageButton play_pause;
+    MediaPlayer mp;
+    ImageView image;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play_pause = findViewById(R.id.btnplay);
+        image= findViewById(R.id.imageView5);
+        mp=MediaPlayer.create(this, R.raw.sorry);
+//sorry es la cancion mp3, dentro del directorio raw
+        image.setImageResource(R.drawable.imgpokemon);
+        play_pause.setBackgroundResource(R.drawable.playpause);
+        play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mp.isPlaying())
+                {
+                    mp.pause();
+                    Toast.makeText(MainActivity.this,"Pausa", Toast.LENGTH_SHORT).show();
+                } else
+                    mp.start();
+
+                Toast.makeText(MainActivity.this,"Play", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }}
